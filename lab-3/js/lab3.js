c1a3d6f34754508b5ac8fd148addadccab395848
kashefinterior-tech <kashef.interior@gmail.com>
2026-03-18 19:49:35 -0400

    tabs+panels document.querySelectorAll

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -1,7 +1,10 @@
 document.addEventListener("DOMContentLoaded",()=>{
+const tabs=document.querySelectorAll(".tabs a");
+const panels=document.querySelectorAll('.tab [role="tabpanel"]');
 
 
 
 
-    
+
+
 })
