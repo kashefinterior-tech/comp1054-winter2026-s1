a1e4f05f97fdb0f56b6203f63aa3c34176cc65fc
kashefinterior-tech <kashef.interior@gmail.com>
2026-03-18 19:54:33 -0400

    added function

--- lab-3/js/lab3.js
+++ lab-3/js/lab3.js
@@ -1,10 +1,20 @@
 document.addEventListener("DOMContentLoaded",()=>{
 const tabs=document.querySelectorAll(".tabs a");
-const panels=document.querySelectorAll('.tab [role="tabpanel"]');
+const panels=document.querySelectorAll('.tabs [role="tabpanel"]');
 
+function showTab(targetId) {
+		tabs.forEach(tab => tab.classList.remove("active"));
+		panels.forEach(panel => panel.classList.remove("show"));
 
+        const activeTab = document.querySelector(`.tabs a[href="${targetId}"]`);
+		const activePanel = document.querySelector(targetId);
 
+        if (activeTab) {
+			activeTab.classList.add("active");
+		}
 
-
-
-})
+		if (activePanel) {
+			activePanel.classList.add("show");
+		}
+    }
+});
